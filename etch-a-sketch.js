34b15fedbc3d176068be844339e4dd1c2b7c00ce
Filip <filolss@protonmail.com>
2023-01-07 01:29:48 +0100

    Extend createGrid to remove old grids

--- etch-a-sketch.js
+++ etch-a-sketch.js
@@ -1,11 +1,17 @@
-function getGrid(){
+function createGrid(dimension){
+  (() => {
+    const grid = document.querySelector('.grid');
+    if (grid) grid.remove();
+  })();
+
   const grid = document.createElement('div');
   grid.classList.add('grid');
-  for (let i = 0; i < 100; i++){
+
+  for (let i = 0; i < dimension; i++){
     const row = document.createElement('div');
     row.classList.add('row');
 
-    for (let j = 0; j < 100; j++){
+    for (let j = 0; j < dimension; j++){
       const square = document.createElement('div');
       square.classList.add('square');
       row.appendChild(square);
@@ -14,7 +20,8 @@ function getGrid(){
     grid.appendChild(row);
   }
   
-  return grid;
+  const body = document.querySelector('body');
+  body.appendChild(grid);
 }
 
 function resetGrid(){
@@ -22,6 +29,4 @@ function resetGrid(){
   squares.forEach(square => square.style.backgroundColor = '#e6e3e3');
 }
 
-const grid = getGrid();
-const body = document.querySelector('body');
-body.appendChild(grid);
+createGrid(100);
